761b0b563b9b8be2150958ac8bf62a04a1e1b665
Joe Vande Vusse <joevandevusse@gmail.com>
2020-11-14 01:09:31 -0600

    Trying google app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var path = require("path");
 var express = require("express");
 var app = express();
-var port = process.env.port || 80;
-var http = require("http").Server(app).listen(port);
+//var port = process.env.port || 80;
+const PORportT = process.env.PORT || 8080;
+//var http = require("http").Server(app).listen(port);
 
 app.use("/css", express.static("./css"));
 app.use("/img", express.static("./img"));
@@ -25,4 +26,9 @@ app.get("/categories", function(req, res) {
 //     var port = server.address().port;
 //     console.log("Listening on http://" + host + ": " + port + "/");
 // });
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    console.log('Press Ctrl+C to quit.');
+  });
 
