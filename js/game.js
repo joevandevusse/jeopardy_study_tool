d19bc5040543bc01a806fb43cb7b145d715b2cb7
Joe Vande Vusse <joevandevusse@gmail.com>
2020-11-16 21:09:49 -0600

    English monarchs and bug fixes

--- js/game.js
+++ js/game.js
@@ -48,7 +48,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         // Go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign('/pages/end.html');
     }
 
     // Empty form text
