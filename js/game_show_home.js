1227a0d9d2488ea318243be7deaa79e2be2c9446
Joe Vande Vusse <joevandevusse@Joes-MacBook-Air.local>
2021-12-20 16:43:45 -0600

    Can pick a game and play just by hitting Enter

--- js/game_show_home.js
+++ js/game_show_home.js
@@ -3,3 +3,13 @@ submitGameId = () => {
     localStorage.setItem("gameId", gameId);     
     window.location.assign("/pages/game_show.html")
 }
+
+// Submit by pressing "Enter" or clicking "Play"
+document.onkeydown = function (e) {
+    var buttonEvent = e || window.event;
+    switch (buttonEvent.which || buttonEvent.keyCode) {
+        case 13 : 
+            e.preventDefault();
+            submitGameId();
+    }
+}
