4bdcaad409f608c480ccdd620fc53e542ad34de5
Joe Vande Vusse <joevandevusse@Joes-MacBook-Air.local>
2022-02-08 16:00:41 -0600

    Removed comments

--- games/get_game_dates.py
+++ games/get_game_dates.py
@@ -11,29 +11,6 @@ import math
 pp = pprint.PrettyPrinter(indent = 4)
 
 def main():
-    # first_game = 7141
-    # #last_game_plus_1 = 7259
-    # last_game_plus_1 = 7190
-    # date_to_game_json = {}
-
-    # for game_id in range(first_game, last_game_plus_1):
-    #     url = "http://j-archive.com/showgame.php?game_id=" + str(game_id)
-    #     u_client = u_req(url)
-    #     page_html = u_client.read()
-    #     u_client.close()
-    #     page_soup = soup(page_html, "html.parser")
-
-    #     if game_id % 10 == 0:
-    #         print(game_id)
-
-    #     title = page_soup.find("title").getText()
-    #     date = title.split()[-1]
-    #     date_to_game_json[date] = game_id
-    
-    # file_name = "date_to_game_id.json"
-    # with open(file_name, "w") as output_file:
-    #     json.dump(date_to_game_json, output_file)
-
     # Let's try it all in one request
     date_to_game_json = {}
     season = 38
